399da59ff881de82f55f5ee3d0f0558563f8f1cf
zVeran <agustavodavid94@gmail.com>
2023-12-01 20:24:54 -0300

    Design 90% finalizado

---
+++ app/src/main/java/com/polarys/appleitour/screenjourney.java
@@ -0,0 +1,14 @@
+package com.polarys.appleitour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screenjourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screenjourney);
+    }
+}
---
+++ app/src/main/java/com/polarys/appleitour/secondsplash.java
@@ -0,0 +1,14 @@
+package com.polarys.appleitour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondsplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondsplash);
+    }
+}
